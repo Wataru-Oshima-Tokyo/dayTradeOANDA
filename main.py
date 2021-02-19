473188b427d32e8d9134c39b2388ebab6b6917db
Wataru <wataruoshima@my.smccd.edu>
2021-02-19 21:57:56 +0900

    this is a test 3

--- main.py
+++ main.py
@@ -593,69 +593,70 @@ class monitorExchangeRate:
             else:
                 sleep(2.7)
 
-def job2():
-    todays_date ="0"
-    while (todays_date != "20220219"):
-        now = datetime.now()
-        todays_date = str(now.strftime("%Y%m%d"))  
-        #10時の東京市場順張りチェック
-        current_time = int(now.strftime("%H%M%S"))
-        if (current_time >= 100500):
-            if(current_time <=110000):
-                decisionMaking.mainexecuting("h10", now, todays_date)
-            elif(current_time <=120000):
-                decisionMaking.mainexecuting("h11", now, todays_date)
-            elif(current_time <=130000):
-                decisionMaking.mainexecuting("h12", now, todays_date)
-            elif(current_time <=134500):
-                decisionMaking.mainexecuting("h13", now, todays_date)
+class main:
+    def job2(self):
+        todays_date ="0"
+        while (todays_date != "20220219"):
+            now = datetime.now()
+            todays_date = str(now.strftime("%Y%m%d"))  
+            #10時の東京市場順張りチェック
+            current_time = int(now.strftime("%H%M%S"))
+            if (current_time >= 100500):
+                if(current_time <=110000):
+                    decisionMaking.mainexecuting("h10", now, todays_date)
+                elif(current_time <=120000):
+                    decisionMaking.mainexecuting("h11", now, todays_date)
+                elif(current_time <=130000):
+                    decisionMaking.mainexecuting("h12", now, todays_date)
+                elif(current_time <=134500):
+                    decisionMaking.mainexecuting("h13", now, todays_date)
+                else:
+                    pass
             else:
                 pass
-        else:
-            pass
-        #15時の東京市場逆張りチェック
-        if (current_time >= 140500):
-            if (current_time <= 142500):
-                decisionMaking.mainexecuting("h14", now, todays_date)
+            #15時の東京市場逆張りチェック
+            if (current_time >= 140500):
+                if (current_time <= 142500):
+                    decisionMaking.mainexecuting("h14", now, todays_date)
+                else:
+                    pass
             else:
-                pass
-        else:
-            pass       
+                pass       
 
-        #18時のロンドン市場順張りチェック
-        if (current_time >= 180500):
-            if(current_time <=190000):
-                decisionMaking.mainexecuting("h18", now, todays_date)
-            elif(current_time <=200000):
-                decisionMaking.mainexecuting("h19", now, todays_date)
-            elif(current_time <=204500):
-                decisionMaking.mainexecuting("h20", now, todays_date)
+            #18時のロンドン市場順張りチェック
+            if (current_time >= 180500):
+                if(current_time <=190000):
+                    decisionMaking.mainexecuting("h18", now, todays_date)
+                elif(current_time <=200000):
+                    decisionMaking.mainexecuting("h19", now, todays_date)
+                elif(current_time <=204500):
+                    decisionMaking.mainexecuting("h20", now, todays_date)
+                else:
+                    pass
             else:
                 pass
-        else:
-            pass
 
-        #21時のロンドン市場逆張りチェック
-        if (current_time >= 210500):
-            if (current_time <= 212500):
-                decisionMaking.mainexecuting("h21", now, todays_date)
+            #21時のロンドン市場逆張りチェック
+            if (current_time >= 210500):
+                if (current_time <= 212500):
+                    decisionMaking.mainexecuting("h21", now, todays_date)
+                else:
+                    pass
             else:
-                pass
-        else:
-            pass 
-        # 25時のニューヨーク市場逆張りチェック
-        if (current_time >= 500):
-            if (current_time <= 1500):
-                decisionMaking.mainexecuting("h01", now, todays_date)
+                pass 
+            # 25時のニューヨーク市場逆張りチェック
+            if (current_time >= 500):
+                if (current_time <= 1500):
+                    decisionMaking.mainexecuting("h01", now, todays_date)
+                else:
+                    pass
             else:
-                pass
-        else:
-            pass 
+                pass 
 
-        print(current_time)
-        sleep(5)
+            print(current_time)
+            sleep(5)
 
 job1 = Thread(target=monitorExchangeRate.job1)
-job2 = Thread(target=job2)
+job2 = Thread(target=main.job2)
 job1.start()
 job2.start()
