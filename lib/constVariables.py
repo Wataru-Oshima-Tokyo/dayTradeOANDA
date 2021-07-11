d808781a305f5da85ed3a5be9526e3fbf6001f4d
Wataru <wataruoshima@my.smccd.edu>
2021-07-11 19:25:31 +0900

    first commit

--- lib/constVariables.py
+++ lib/constVariables.py
@@ -1,10 +1,12 @@
 class CVAL:
-    my_addr = "bigisland.business@gmail.com"
-    my_pass = "vtimcmvrnktbxyhp"
-    access_token = "aeb76294b5192c55779fef4fe56eaf75-45bb64a56aad2a6b6dd22062e59027e3"
-    accountID = "101-001-18324553-001" 
-    host="ec2-50-16-108-41.compute-1.amazonaws.com",
-    database="d382g5qv7f30fj",
-    user="ayeviyfmhbuktr",
-    password="46cee268a578eeb2c75271334baf539bf3cedaa9fb508f52a04e26e069c29d72",
-    port="5432"
+    my_addr = "******@gmail.com" #your email address
+    my_pass = "*********"  #your password for the second appllicaton password on gmail agress
+    access_token = "***************"  #Your OANADA API's access-token
+    accountID = "**********" #Your account ID of OANDA
+
+    #postgreds DB info (all info are provided automatically)
+    host="*********.amazonaws.com" #its host name
+    database="********"  #its database name
+    user="*******"  #its user
+    password="********" # its password
+    port="****"  #its port
